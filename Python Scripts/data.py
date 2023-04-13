dd4fdfc8366490e82f173d2e61f920843fc28cc5
K-Lenin279 <leninkagema72@gmail.com>
2023-04-13 12:56:55 +0300

    playground init

---
+++ Python Scripts/data.py
@@ -0,0 +1,9 @@
+char_name = "John"
+char_age = "35"
+# is_male = True // boolean values
+print("This is " + char_name + ".")
+print("He is " + char_age + " years old.")
+
+char_name = "Mike"
+print("This is " + char_name + ".")
+print(char_name + ", is " + char_age + " years old.")
---
+++ Python Scripts/hello.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+msg = "Hey There!"
+print(msg)
+print(msg.lower())
+print(msg.upper())
+print(msg.upper().isupper())
+print(len(msg))
+print(msg[0]) #string index starts at zero[0]
+print(msg.index("!"))
+print(msg.replace("Hey", "Konnichiwa"))
+print (msg + "Welcome to the Spyder IDE\n")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
